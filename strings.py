1d114834d705d37b3428f046ce50deefb29f3246
EugeneShell <58475382+EugeneShell@users.noreply.github.com>
2021-09-30 10:44:55 +0300

    Add files via upload
    comments were added

--- strings.py
+++ strings.py
@@ -4,7 +4,7 @@ Created on Sun Sep 26 20:32:21 2021
 
 @author: Yauheny_Shaludzko
 """
-
+# at first let put text into the variable
 text = '''
 
   tHis iz your homeWork, copy these Text to variable.
@@ -21,14 +21,13 @@ text = '''
 
   last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, not only Spaces, but ALL whitespaces. I got 87.'''
 
-sentences = text.split(".")
-sentfixed = [i.strip(' \t\n\r').capitalize() for i in sentences]
-addsent = []
+sentences = text.split(".")  # split text to sentences on dots
+sentfixed = [i.strip(' \t\n\r').capitalize() for i in sentences]  # trim whitespases and capitalize sentences via gen
+addsent = []  # creating empty list for additional sentence
 for i in sentfixed:
-    wordlist = i.split(' ')
-    addsent.append(wordlist[-1])
-addsent1 = ' '.join(addsent).capitalize().rstrip() + '.'
-next_text = ('. '.join(sentfixed) + addsent1).replace('iz', 'is')
+    wordlist = i.split(' ')  # splitting sentence to words
+    addsent.append(wordlist[-1])  # getting the last word and appending it to the list
+addsent1 = ' '.join(addsent).capitalize().rstrip() + '.'  # creating additional sentence with join&capitalize
+next_text = ('. '.join(sentfixed) + addsent1).replace('iz', 'is')  # replacing iz/is
 print(next_text)
-print('Number of whitespace characters in given text:', text.count(' ') + text.count('\n '))
-
+print('Number of whitespace characters in given text:', text.count(' ') + text.count('\n '))  # counting whitespaces
